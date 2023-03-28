f6b4f9f5001702c1182ea3d25b7bf7ac645e420f
yunisaliosman <yunis.osman@student.moringaschool.com>
2023-03-28 14:52:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
